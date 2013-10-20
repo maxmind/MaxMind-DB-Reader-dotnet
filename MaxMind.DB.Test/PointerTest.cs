e2aa6943dda88b0d2738b38e8ea11338eca3e6a3
Brian Sullivan <bmsullivan@gmail.com>
2013-10-20 16:29:35 -0500

    Rename folders

---
+++ MaxMind.DB.Test/PointerTest.cs
@@ -0,0 +1,37 @@
+﻿namespace MaxMind.DB.Test
+{
+    using System.IO;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class PointerTest
+    {
+        [Test]
+        public void TestWithPointers()
+        {
+            var stream = new MemoryStream(File.ReadAllBytes("..\\..\\TestData\\MaxMind-DB\\test-data\\maps-with-pointers.raw"));
+            using (stream)
+            {
+                var decoder = new Decoder(stream, 0);
+
+                var node = decoder.Decode(0).Node;
+                Assert.That(node.Value<string>("long_key"), Is.EqualTo("long_value1"));
+
+                node = decoder.Decode(22).Node;
+                Assert.That(node.Value<string>("long_key"), Is.EqualTo("long_value2"));
+
+                node = decoder.Decode(37).Node;
+                Assert.That(node.Value<string>("long_key2"), Is.EqualTo("long_value1"));
+
+                node = decoder.Decode(50).Node;
+                Assert.That(node.Value<string>("long_key2"), Is.EqualTo("long_value2"));
+
+                node = decoder.Decode(55).Node;
+                Assert.That(node.Value<string>("long_key"), Is.EqualTo("long_value1"));
+
+                node = decoder.Decode(57).Node;
+                Assert.That(node.Value<string>("long_key2"), Is.EqualTo("long_value2"));
+            }
+        }
+    }
+}
