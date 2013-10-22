e785eb8be057f043bd6ce4bf727275e2ebc692e0
Gregory Oschwald <goschwald@maxmind.com>
2013-10-22 07:43:00 -0700

    Updated build information and added nuspec file

--- MaxMind.DB/Properties/AssemblyInfo.cs
+++ MaxMind.DB/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MaxMind.GeoIP2")]
-[assembly: AssemblyDescription(".NET client for MaxMind GeoIP database version 2")]
+[assembly: AssemblyTitle("MaxMind.DB")]
+[assembly: AssemblyDescription(".NET reader for the MaxMind DB file format")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Rage4 Networks Limited")]
-[assembly: AssemblyProduct("MaxMind.GeoIP2")]
-[assembly: AssemblyCopyright("Copyright © Rage4 Networks Limited 2013")]
+[assembly: AssemblyCompany("MaxMind, Inc.")]
+[assembly: AssemblyProduct("MaxMind.DB")]
+[assembly: AssemblyCopyright("Copyright © 2013 MaxMind, Inc.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("15fb5fc3-afeb-456b-b74f-1a11561530dc")]
+[assembly: Guid("044a98c5-0d88-4273-a769-ef4c72cf1bd9")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
 [assembly: InternalsVisibleTo("MaxMind.DB.Test")]
