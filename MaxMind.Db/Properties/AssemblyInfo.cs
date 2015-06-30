142f6439bff5d57e0445e37a5edeae92138d4731
Gregory Oschwald <goschwald@maxmind.com>
2015-06-30 12:58:31 -0700

    1.1.0-beta1

--- MaxMind.Db/Properties/AssemblyInfo.cs
+++ MaxMind.Db/Properties/AssemblyInfo.cs
@@ -41,5 +41,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("1.0.1")]
-[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.1.0")]
+[assembly: AssemblyInformationalVersion("1.1.0-beta1")]
 [assembly: InternalsVisibleTo("MaxMind.Db.Test")]
