8034ecf1e71af0a9e96816e955ecf859a2c99da2
Kevin Lewis <kevin@lewistech.com>
2016-09-12 12:41:00 -0700

    Make MemoryMappedFile load into global namespace and set security to allow all - closes #33

--- MaxMind.Db/MemoryMapBuffer.cs
+++ MaxMind.Db/MemoryMapBuffer.cs
@@ -24,7 +24,7 @@ namespace MaxMind.Db
         {
             // Ideally we would use the file ID in the mapName, but it is not
             // easily available from C#.
-            var mapName = $"{fileInfo.FullName.Replace("\\", "-")}-{Length}";
+            var mapName = $"Global\\{fileInfo.FullName.Replace("\\", "-")}-{Length}";
             lock (FileLocker)
             {
                 try
@@ -40,8 +40,14 @@ namespace MaxMind.Db
                     var stream = new FileStream(file, FileMode.Open, FileAccess.Read,
                                                 FileShare.Delete | FileShare.Read);
 #if !NETSTANDARD1_4
+                    var security = new MemoryMappedFileSecurity();
+                    security.AddAccessRule(
+                        new System.Security.AccessControl.AccessRule<MemoryMappedFileRights>(
+                            new System.Security.Principal.SecurityIdentifier(System.Security.Principal.WellKnownSidType.WorldSid, null),
+                            MemoryMappedFileRights.Read,
+                            System.Security.AccessControl.AccessControlType.Allow));
                     _memoryMappedFile = MemoryMappedFile.CreateFromFile(stream, mapName, Length,
-                            MemoryMappedFileAccess.Read, null, HandleInheritability.None, false);
+                            MemoryMappedFileAccess.Read, security, HandleInheritability.None, false);
 #else
 
                     // In .NET Core, named maps are not supported for Unices yet: https://github.com/dotnet/corefx/issues/1329
