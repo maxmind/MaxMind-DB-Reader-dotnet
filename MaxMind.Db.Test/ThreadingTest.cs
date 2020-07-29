d532ee8f640d373a33fa2e8c1968136eeeaae890
Gregory Oschwald <oschwald@gmail.com>
2020-07-29 12:13:33 -0700

    Use simple using statements

--- MaxMind.Db.Test/ThreadingTest.cs
+++ MaxMind.Db.Test/ThreadingTest.cs
@@ -26,31 +26,29 @@ namespace MaxMind.Db.Test
         {
             var ipsAndResults = new Dictionary<IPAddress, string?>();
             var rand = new Random();
-            using (var reader = new Reader(_testDatabase, mode))
+            using var reader = new Reader(_testDatabase, mode);
+            for (var i = 0; i < 10000; i++)
             {
-                for (var i = 0; i < 10000; i++)
-                {
-                    var ip = new IPAddress(rand.Next(int.MaxValue));
-                    var resp = reader.Find<object>(ip);
-                    if (resp == null || ipsAndResults.ContainsKey(ip)) continue;
-                    ipsAndResults.Add(ip, resp.ToString());
-                }
-
-                var ips = ipsAndResults.Keys.ToArray();
-                Parallel.For(0, ips.Length, i =>
-                {
-                    var ipAddress = ips[i];
-                    var result = reader.Find<object>(ipAddress);
-                    if (result == null)
-                    {
-                        throw new Xunit.Sdk.XunitException("unexpected null result value");
-                    }
-                    var resultString = result.ToString();
-                    var expectedString = ipsAndResults[ipAddress];
-                    if (resultString != expectedString)
-                        throw new Exception($"Non-matching result. Expected {expectedString}, found {resultString}");
-                });
+                var ip = new IPAddress(rand.Next(int.MaxValue));
+                var resp = reader.Find<object>(ip);
+                if (resp == null || ipsAndResults.ContainsKey(ip)) continue;
+                ipsAndResults.Add(ip, resp.ToString());
             }
+
+            var ips = ipsAndResults.Keys.ToArray();
+            Parallel.For(0, ips.Length, i =>
+            {
+                var ipAddress = ips[i];
+                var result = reader.Find<object>(ipAddress);
+                if (result == null)
+                {
+                    throw new Xunit.Sdk.XunitException("unexpected null result value");
+                }
+                var resultString = result.ToString();
+                var expectedString = ipsAndResults[ipAddress];
+                if (resultString != expectedString)
+                    throw new Exception($"Non-matching result. Expected {expectedString}, found {resultString}");
+            });
         }
 
         [Theory]
@@ -62,10 +60,8 @@ namespace MaxMind.Db.Test
         {
             Parallel.For(0, 50, i =>
             {
-                using (var reader = new Reader(_testDatabase, mode))
-                {
-                    reader.Find<object>(IPAddress.Parse("175.16.199.0"));
-                }
+                using var reader = new Reader(_testDatabase, mode);
+                reader.Find<object>(IPAddress.Parse("175.16.199.0"));
             });
         }
     }
